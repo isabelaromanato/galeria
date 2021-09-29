ac624227c382ed5154bdbc2495b850350c1b5d83
ISABELA VICTORIA DE NOVAIS ROMANATO <21193062@CFP127ALUNO.LOCAL>
2021-09-29 11:03:22 -0300

    efeito de slide na galeria

--- galeria.js
+++ galeria.js
@@ -12,11 +12,14 @@ const imagens = [
 
 ]
 
+//replace -> substituir | split ->criar um array
+const limparId = (url) => url.replace("./img/", "").split(".")[0].replace(" ", "-")
+
 const criarItem = (urlmagem) => {
     const container = document.querySelector(".galeria-container")
 
     const novoLink = document.createElement("a")
-    novoLink.href = urlmagem 
+    novoLink.href = `#${limparId(urlmagem)}`
     novoLink.classList.add("galeria-itens")
     novoLink.innerHTML = `<img src= "${urlmagem}" alt="">`
 
@@ -28,7 +31,36 @@ const criarItem = (urlmagem) => {
 // </a>
  //não é correto//     `
 }
-const carregarImagens = () =>  imagens.forEach(criarItem)
+const carregarGaleria = (imagens) =>  imagens.forEach(criarItem)
 
-carregarImagens()
+const criarSlide = (urlImagem, indice, arr) => {
+    const container = document.querySelector(".slide-container")
+    const novoDiv = document.createElement("div")
+    novoDiv.classList.add("slide")
+    novoDiv.id = limparId(urlImagem)
+
+    const indiceAnterior = indice <=0 ? arr.length -1 : indice -1
+    const idAnterior = limparId(arr[indiceAnterior])
+
+    const indiceProximo = indice >= arr.length -1 ? 0 : indice +1
+    const idProximo = limparId(arr[indiceProximo]) 
+
+    novoDiv.innerHTML = `
+    <div class="imagem-container">
+    <a href="" class="icones fechar">&#10006;</a>
+    <a href="#${idAnterior}" class="icones anterior">&#171;</a>
+    <img src="${urlImagem}" alt="">
+    <a href="#${idProximo}" class="icones proximo">&#187;</a>
+    </div>
+    `
+
+    container.appendChild(novoDiv)
+}
+
+const carregarSlide = (imagens) => imagens.forEach(criarSlide)
+
+carregarGaleria (imagens)
+carregarSlide(imagens)
+
+
 
